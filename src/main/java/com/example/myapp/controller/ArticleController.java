38c606eb413f53ae93a41a9659013a0318e12fd6
homkim <homkim01@gmail.com>
2021-08-08 21:34:30 +0900

    2134 목록 가져와서 표시

--- src/main/java/com/example/myapp/controller/ArticleController.java
+++ src/main/java/com/example/myapp/controller/ArticleController.java
@@ -4,27 +4,37 @@ import com.example.myapp.dto.ArticleForm;
 import com.example.myapp.entity.Article;
 import com.example.myapp.repository.ArticleRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@RequiredArgsConstructor // final 필드 값을 알아서 가져옴(@Autowired 대체)
 @Slf4j
 public class ArticleController {
     
-    @Autowired
-    private ArticleRepository articleRepository;
+    // @Autowired -> @RequiredArgsConstructor로 대체 : !!final 붙여야 함!!
+    private final ArticleRepository articleRepository;
 
     @GetMapping("/articles")
-    public String articleList(){
+    public String index(Model model){ //뷰로 전달하기 위한 모델 객체 자동삽입
+        // 모든 Article을 가져옴
+        // Iterable 인터페이스는 ArrayList의 부모 인터페이스
+        Iterable<Article> articleList = articleRepository.findAll();
 
+        // 뷰페이지로 articles 전달
+        model.addAttribute("articles", articleList);
+
+        // 뷰페이지 설정
         return "articles/index";
     }
 
-    @PostMapping("/articles/new")
+    @GetMapping("/articles/new")
     public String newArticleForm(){
 
         return "articles/new";
